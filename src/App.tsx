08e853df85af18673d53f286db149a89e2d20cf9
stefi <stefaniaman314@gmail.com>
2023-04-09 20:03:52 +0300

    Added footer.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HeroLayout from './layouts/hero';
 import GalleryLayout from './layouts/gallery';
+import FooterLayout from './layouts/footer';
 
 function App() {
   return (
     <div className="App">
       <HeroLayout/>
       <GalleryLayout/>
+      <FooterLayout/>
     </div>
   );
 }
